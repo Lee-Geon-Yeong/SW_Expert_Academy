940ecb0d4138ad2741f5265855643a87aa5ae499
Lee-Geon-Yeong <yellowisher@naver.com>
2021-01-10 23:10:08 +0900

    coding 1

---
+++ D1/2070_max_min_equal.py
@@ -0,0 +1,26 @@
+T = int(input())
+
+for i in range(T):
+    a, b = list(map(int, input().split()))
+    if a > b:
+        print("#{} >".format(i + 1))
+    elif a == b:
+        print("#{} =".format(i + 1))
+    else:
+        print("#{} <".format(i + 1))
+
+
+# def solution(a, b):
+#     if a > b:
+#         return '>'
+#     elif a == b:
+#         return '='
+#     else:
+#         return '<'
+#
+#
+# if __name__ == '__main__':
+#     cnt = int(input())
+#     for i in range(cnt):
+#         a, b = list(map(int, input().split()))
+#         print("#%d %s" % (i + 1, solution(a, b)))
