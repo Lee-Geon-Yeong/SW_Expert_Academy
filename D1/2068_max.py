1554b85cb9496538be66ff30fe1435a0deb7a198
Lee-Geon-Yeong <59759468+Lee-Geon-Yeong@users.noreply.github.com>
2021-01-19 20:10:24 +0900

    Implementation

--- D1/2068_max.py
+++ D1/2068_max.py
@@ -3,4 +3,4 @@ T=int(input())
 for i in range(T):
     output_list=list(map(int, input().split()))
     output_max=max(output_list)
-    print(f'#{i+1} {output_max}')
+    print(f'#{i+1} {output_max}') 
